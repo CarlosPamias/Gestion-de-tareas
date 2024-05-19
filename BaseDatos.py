f7ae5554c83a873aa7830442ef18a92ed4d4a46f
Carlos Pamias Mora <cpamias9@gmail.com>
2024-05-19 18:56:29 +0200

    Fase 1

--- BaseDatos.py
+++ BaseDatos.py
@@ -8,39 +8,18 @@ from tkinter import messagebox as mb
 
 class BDatos:
 
-    def __init__(self, codigo, tematica, academia, prioridad, descripcion, fecha_inicio, fecha_fin_prevista):
-        self.codigo = codigo
-        self.tematica = tematica
-        self.academia = academia
-        self.prioridad = prioridad
-        self.descripcion = descripcion
-        self.fecha_inicio = fecha_inicio
-        self.fecha_fin_prevista = fecha_fin_prevista
-        self.fecha_fin = None  # Inicialmente sin fecha de finalización
-
+#Inicializamos la tabla
+    def __init__(self,):
         self.conexion = sqlite3.connect("tareas.db") #Conexion
-        cursor = self.conexion.cursor() # Creamos cursos
-        cursor.execute('''CREATE TABLE IF NOT EXISTS tareas (
-                        codigo INTEGER PRIMARY KEY,
-                        tematica TEXT,
-                        Academia TEXT,
-                        Prioridad TEXT,
-                        Descripcion TEXT,
-                        FechaInicio DATE,
-                        FechaFinPrevista DATE,
-                        FechaFin DATE,
-                    
-                    )''')
-        self.conexion.commit()  # Guardamos los datos
-        cursor.close()
+      
               
   #Funciones para interactuar con la base de datos:
 
-    def agregar_tarea(self,tematica, academia, prioridad, descripcion, fecha_inicio, fecha_fin_prevista):
+    def agregar_tarea(self,tematica, academia, prioridad, descripcion, fecha_inicio, fecha_fin_prevista,fecha_fin):
         try:
             cursor = self.conexion.cursor() 
-            bd=('''INSERT INTO tareas (tematica, academia, prioridad, descripcion, fecha_inicio, fecha_fin_prevista) VALUES ({},{},{},{},{},{})'''.format
-                (tematica, academia, prioridad, descripcion, fecha_inicio, fecha_fin_prevista))
+            bd=('''INSERT INTO tareas (tematica, academia, prioridad, descripcion, fecha_inicio, fecha_fin_prevista,fecha_fin) VALUES ({},{},{},{},{},{},{})'''.format
+                (tematica, academia, prioridad, descripcion, fecha_inicio, fecha_fin_prevista,fecha_fin))
             cursor.execute(bd)
             self.conexion.commit()
             cursor.close()
@@ -48,7 +27,7 @@ class BDatos:
         except Exception as e:
             mb.showerror("Error", f"Error al agregar la tarea: {e}")
 
-    def obtener_todas_las_tareas(cursor):
+    def obtener_todas_las_tareas(self):
         try:
             cursor = self.conexion.cursor() 
             bd=("SELECT * FROM tareas")
@@ -58,14 +37,16 @@ class BDatos:
         except Exception as e:
             mb.showerror("Error", f"Error al obtener las tareas: {e}")
            
-     def modificar_tarea(self,codigo,tematica, academia, prioridad, descripcion, fecha_inicio, fecha_fin_prevista):
+    def modificar_tarea(self,codigo,tematica, academia, prioridad, descripcion, fecha_inicio, fecha_fin_prevista,fecha_fin):
         try:
             cursor = self.conexion.cursor() 
-            bd=('''UPDATE tareas SET tematica = {}, academia = {}, prioridad = {}, descripcion= {}, fecha_inicio= {}, fecha_fin_prevista= {}WHERE codigo = {})'''.format
-                (tematica, academia, prioridad, descripcion, fecha_inicio, fecha_fin_prevista,codigo))
+            bd=('''UPDATE tareas SET tematica = {}, academia = {}, prioridad = {}, descripcion= {}, fecha_inicio= {}, fecha_fin_prevista= {},fecha_fin ={} WHERE codigo = {})'''.format
+                (tematica, academia, prioridad, descripcion, fecha_inicio, fecha_fin_prevista,fecha_fin,codigo))
             cursor.execute(bd)
+            datos = cursor.rowcount
             self.conexion.commit()
             cursor.close()
+            return datos
             mb.showinfo("Tarea agregada", "La tarea se ha agregado correctamente")
         except Exception as e:
             mb.showerror("Error", f"Error al agregar la tarea: {e}")
--- main.py
+++ main.py
@@ -12,10 +12,15 @@ class Ventana(Frame):
         super().__init__(master)
 
         self.codigo = StringVar()
-        self.nombre = StringVar()
+        self.tematica = StringVar()
+        self.academia = StringVar()
+        self.prioridad = StringVar()        
         self.descripcion = StringVar()
-        self.estado = StringVar()
+        self.fecha_inicio = StringVar()
+        self.fecha_fin_prevista = StringVar()
+        self.fecha_fin = StringVar()
 
+#        Adaptamos la pantalla
         self.master.columnconfigure(0, weight=1)
         self.master.rowconfigure(0,weight=1)
         self.master.rowconfigure(1,weight=5)
@@ -44,19 +49,147 @@ class Ventana(Frame):
 
         Label(self.frame_uno, text='Agregar y actualizad datos', bg='white', fg='black',
             font=('Kaufmann BT', '13', 'bold')).grid(columnspan=2,column=0,row=0,pady=5)
+        
         Label(self.frame_uno, text= 'Tematica', fg='black',bg= 'white',
             font=('Rockwell',13,'bols')).grid(column=0,row=1,pady=5)
+        Label(self.frame_uno, text= 'Academia', fg='black',bg= 'white',
+            font=('Rockwell',13,'bols')).grid(column=0,row=2,pady=5)
+        Label(self.frame_uno, text= 'Prioridad', fg='black',bg= 'white',
+            font=('Rockwell',13,'bols')).grid(column=0,row=3,pady=5)
+        Label(self.frame_uno, text= 'Descripción', fg='black',bg= 'white',
+            font=('Rockwell',13,'bols')).grid(column=0,row=4,pady=5)
+        Label(self.frame_uno, text= 'Fecha inicio', fg='black',bg= 'white',
+            font=('Rockwell',13,'bols')).grid(column=0,row=5,pady=5)
+        Label(self.frame_uno, text= 'Fecha Fin', fg='black',bg= 'white',
+            font=('Rockwell',13,'bols')).grid(column=0,row=6,pady=5)    
 
 #creamos las cajas de entrada de la informacion
         Entry(self.frame_uno, textvariable=self.tematica, font=('Comic Sans MS', 12),
               highlightbackground='white',highlightthickness=5).grid(colum=1,row=1)
+        Entry(self.frame_uno, textvariable=self.academia, font=('Comic Sans MS', 12),
+              highlightbackground='white',highlightthickness=5).grid(colum=1,row=2)
+        Entry(self.frame_uno, textvariable=self.prioridad, font=('Comic Sans MS', 12),
+              highlightbackground='white',highlightthickness=5).grid(colum=1,row=3)
+        Entry(self.frame_uno, textvariable=self.descripcion, font=('Comic Sans MS', 12),
+              highlightbackground='white',highlightthickness=5).grid(colum=1,row=4)
+        Entry(self.frame_uno, textvariable=self.fecha_inicio, font=('Comic Sans MS', 12),
+              highlightbackground='white',highlightthickness=5).grid(colum=1,row=5)
+        Entry(self.frame_uno, textvariable=self.fecha_fin_prevista, font=('Comic Sans MS', 12),
+              highlightbackground='white',highlightthickness=5).grid(colum=1,row=6)
 
 #creamos los botones para ejecutar acciones
         Button(self.frame_uno,text= 'Añadir datos', font=('arial', 9, 'bold'),bg='white',
                width=20,bd=3,command = self.agregardatos).grid (column=2, row=2, pady=5,padx=5)
-
+        Button(self.frame_uno,text= 'Modificar', font=('arial', 9, 'bold'),bg='white',
+               width=20,bd=3,command = self.agregardatos).grid (column=2, row=3, pady=5,padx=5)
+#creamos el estilo de la tabla 
+        estilo_tabla = ttk.Style()
+        estilo_tabla.configure('Treeview', font= ('Helvetia', 10, 'bold'), foreground='black', background='white')
+        estilo_tabla.map('Treeview',background=['selected', 'white'],foreground=['selected', 'black'])
+        estilo_tabla.configure('Heading',background='white',foreground= black, padding=3,font=('Arial', 10, 'bold'),)
 #Creamos un estilo de tabla para mostrar la información
-        self.tabla[columnas]= ()
+        self.tabla = ttk.Treeview(self.frame_dos)
+        self.tabla.grid(column=0, row=0, sticky='nsew')
+        ladox= ttk.Scrollbar(self.frame_dos,orient='horizontal',command=self.tabla.xview)
+        ladox.grid(column=0,row=1,sticky='ew')
+        ladoy= ttk.Scrollbar(self.frame_dos,orient='vertical',command=self.tabla.yview)
+        ladoy.grid(column=1,row=0,sticky='ns')
+        self.tabla.configure(xscrollcommand= ladox.set, yscrollcommand= ladoy.set)
+
+        self.tabla['columns'] = ('Academia','Prioridad','Descripción','Fecha inicio','Fecha fin')
+        self.tabla.column('#0', minwidth='100',width=120, anchor='center')
+        self.tabla.column('Academia', minwidth='100',width=120, anchor='center')
+        self.tabla.column('Prioridad', minwidth='100',width=120, anchor='center')
+        self.tabla.column('Descripcion', minwidth='100',width=120, anchor='center')
+        self.tabla.column('Fecha inicio', minwidth='100',width=120, anchor='center')
+        self.tabla.column('Fecha fin', minwidth='100',width=120, anchor='center')
+
+        self.tabla.heading('#0', text='Tematica', anchor='centre')
+        self.tabla.heading('Academia', text='Academia', anchor='centre')
+        self.tabla.heading('Prioridad', text='Prioridad', anchor='centre')
+        self.tabla.heading('Descripcion', text='Descripcion', anchor='centre')
+        self.tabla.heading('Fecha inicio', text='Fecha inicio', anchor='centre')
+        self.tabla.heading('Fecha fin', text='Fecha fin', anchor='centre')
+
+        self.tabla.bind('<<TreeviewSelect>>', self.obtener_fila)
+        self.tabla.bind('<Doble-1>', self.eliminar_datos)
+
+    def obtener_fila(self,event):
+        item = self.tabla.focus()
+        self.data =self.tabla.item(item)
+        self.tematica.set(self.data['text'])
+        self.academia.set(self.data['values'][0])
+        self.prioridad.set(self.data['values'][1])
+        self.descripcion.set(self.data['values'][2])
+        self.fecha_inicio.set(self.data['values'][3])
+        self.fecha_fin.set(self.data['values'][4])
+
+    def elininar_datos(self, event):
+        self.limpiar_datos()
+        item = self.tabla.selection()[0]
+        x = messagebox.askquestion('Informacion', '¿Desea eliminar?')
+        if x == 'yes':
+            self.tabla.delete(item)
+            self.base_datos.eliminar_tarea(self.data['text'])
+            
+    def agregar_datos(self):
+        tematica = self.tematica.get()
+        academia = self.academia.get()
+        prioridad = self.prioridad.get()
+        descripcion = self.descripcion.get()
+        fecha_inicio = self.fecha_inicio.get()
+        fecha_fin = self.fecha_fin.get()
+        if tematica and academia and prioridad and descripcion != ' ':
+            self.tabla.insert('',0,text = tematica, value = datos)
+            self.base_datos.agregar_tarea(tematica, academia,prioridad,fecha_inicio,fecha_fin)
+            self.limpiar_campos()
+        
+    def actualizar_tabla(self):
+        self.limpiar_campos()
+        datos = self.base_datos.obtener_todas_las_tareas()
+        self.tabla.delete(*self.tabla.get_children())
+        i = -1
+        for dato in datos:
+            i=i+1
+            self.tabla.insert('', i,text=datos[i][1:2][0], values=datos[i][2:7])
+    
+    def actualizar_datos(self):
+        item = self.tabla.focus()
+        self.data = self.data.item(item)
+        nombre = self.tabla['text']
+        datos = self.base_datos.obtener_todas_las_tareas()
+        for fila in datos:
+            codigo = fila[0]
+            tematica_db = fila[1]
+            if tematica_db == tematica:
+                if codigo != None:
+                    tematica =self.tematica.get()
+                    academia =self.academia.get()
+                    prioridad =self.prioridad.get()
+                    descripcion =self.descripcion.get()
+                    fecha_inicio =self.fecha_inicio.get()
+                    fecha_fin =self.fecha_fin.get()
+                    if tematica and academia and prioridad and descripcion != '':
+                        self.base_datos.modificar_tarea(codigo,tematica,prioridad,descripcion,fecha_inicio,fecha_fin)
+                        self.tabla.delete(*self.tabla.get_children())
+                        datos = self.base_datos.obtener_todas_las_tareas()
+                        i = -1
+                        for dato in datos:
+                            i=i+1
+                            self.tabla.insert('', i,text=datos[i][1:2][0], values=datos[i][2:7])
+    
+    def limpiar_campos(self):
+        self.tematica.set('')
+        self.academia.set('')
+        self.prioridad.set('')
+        self.descripcion.set('')
+        self.fecha_inicio.set('')
+        self.fecha_fin.set('')
+
+           
+    
+
+
 
     if __name__ == "__main__":
         ventana = Tk()
