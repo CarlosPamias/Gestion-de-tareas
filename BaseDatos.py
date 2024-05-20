05efc5408ced324eac7e0a6fabb112ee14de5935
Carlos Pamias Mora <cpamias9@gmail.com>
2024-05-20 10:32:39 +0200

    aa1

--- BaseDatos.py
+++ BaseDatos.py
@@ -15,15 +15,14 @@ class BDatos:
               
   #Funciones para interactuar con la base de datos:
 
-    def agregar_tarea(self,tematica, academia, prioridad, descripcion, fecha_inicio, fecha_fin_prevista,fecha_fin):
+    def agregar_tarea(self,descripcion,tematica, academia, prioridad,  fecha_inicio,fecha_fin):
         try:
             cursor = self.conexion.cursor() 
             bd=('''INSERT INTO tareas (descripcion, tematica, academia, prioridad, fecha_inicio, fecha_fin) VALUES ({},{},{},{},{},{})'''.format
-                (descripcion, tematica, academia, prioridad, fecha_inicio, fecha_fin_prevista,fecha_fin))
+                (descripcion, tematica, academia, prioridad, fecha_inicio, fecha_fin))
             cursor.execute(bd)
             self.conexion.commit()
             cursor.close()
-            mb.showinfo("Tarea agregada", "La tarea se ha agregado correctamente")
         except Exception as e:
             mb.showerror("Error", f"Error al agregar la tarea: {e}")
 
@@ -37,17 +36,16 @@ class BDatos:
         except Exception as e:
             mb.showerror("Error", f"Error al obtener las tareas: {e}")
            
-    def modificar_tarea(self,descripcion, codigo,tematica, academia, prioridad, fecha_inicio, fecha_fin_prevista,fecha_fin):
+    def modificar_tarea(self,descripcion, codigo,tematica, academia, prioridad, fecha_inicio, fecha_fin):
         try:
             cursor = self.conexion.cursor() 
-            bd=('''UPDATE tareas SET descripcion= {}, tematica = {}, academia = {}, prioridad = {}, fecha_inicio= {}, fecha_fin_prevista= {},fecha_fin ={} WHERE codigo = {})'''.format
-                (descripcion,tematica, academia, prioridad, fecha_inicio, fecha_fin_prevista,fecha_fin,codigo))
+            bd=('''UPDATE tareas SET descripcion= {}, tematica = {}, academia = {}, prioridad = {}, fecha_inicio= {}, fecha_fin ={} WHERE codigo = {})'''.format
+                (descripcion,tematica, academia, prioridad, fecha_inicio,fecha_fin,codigo))
             cursor.execute(bd)
             datos = cursor.rowcount
             self.conexion.commit()
             cursor.close()
             return datos
-            mb.showinfo("Tarea agregada", "La tarea se ha agregado correctamente")
         except Exception as e:
             mb.showerror("Error", f"Error al agregar la tarea: {e}")
 
@@ -55,17 +53,11 @@ class BDatos:
     def eliminar_tarea(self, codigo):
         try:
             cursor = self.conexion.cursor() 
-            bd=("DELETE FROM tareas WHERE codigo = {}".format (codigo,))
+            bd=("DELETE FROM tareas WHERE codigo = {}".format (codigo))
             cursor.execute(bd)
             self.conexion.commit()
             cursor.close()
-            mb.showinfo("Tarea eliminada", "La tarea se ha eliminado correctamente")
         except Exception as e:
             mb.showerror("Error", f"Error al eliminar la base de datos: {e}")
     
-    def cerrar_tabla(self):
-        try:
-            self.conexion.close()
-            mb.showinfo("Conexion cerrada", "La conexion se ha cerrado correctamente")
-        except Exception as e:
-            mb.showerror("Error", f"Error al cerrar la tabla: {e}")
+   
